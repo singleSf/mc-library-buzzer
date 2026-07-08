a0c1f5bfe9b65b4d3aa4d3a19f80161bba8d1b73
singleSf <52894032+singleSf@users.noreply.github.com>
2026-07-08 14:10:03 +0300

    Update BUZZER.cpp

--- src/BUZZER.cpp
+++ src/BUZZER.cpp
@@ -1,162 +1,213 @@
-using namespace std;
-
-#ifndef BUZZER_INIT
-#define BUZZER_INIT true
-
 #include "BUZZER.h"
 
-BUZZER::BUZZER(byte _pin) {
+BUZZER::BUZZER(uint8_t _pin) {
   this->__pin = _pin;
-
   this->setEnable(true);
 
-  pinMode(__pin, OUTPUT);
-  digitalWrite(__pin, LOW);
+  pinMode(this->__pin, OUTPUT);
+  digitalWrite(this->__pin, LOW);
 }
 
-BUZZER::~BUZZER() {}
+BUZZER::~BUZZER() {
+}
 
 void BUZZER::setEnable(bool _isEnable) {
   this->__isEnable = _isEnable;
+  if (!this->__isEnable) {
+    this->__toneEnd();
+  }
 }
 
-void BUZZER::__toneStart(uint32_t _frequency) {
-  if(!this->__isEnable){
-    return;
+void BUZZER::__toneStart(uint16_t _frequency) {
+  if (this->__isEnable) {
+    tone(this->__pin, _frequency);
   }
-  
-  tone(this->__pin, _frequency);
 }
 
 void BUZZER::__toneEnd() {
-  if(!this->__isEnable){
-    return;
-  }
-  
   noTone(this->__pin);
 }
 
-void BUZZER::__delay(uint32_t _milliSeconds, uint32_t _microSeconds) {
-  if(!this->__isEnable){
-    return;
-  }
-  
-  if(_milliSeconds){
+void BUZZER::__delay(uint16_t _milliSeconds, uint16_t _microSeconds) {
+  if (_milliSeconds > 0) {
     delay(_milliSeconds);
-  }else if(_microSeconds){
+  } else if (_microSeconds > 0) {
     delayMicroseconds(_microSeconds);
   }
 }
 
-void BUZZER::__end(uint32_t _delay) {
+void BUZZER::__end(uint16_t _delayAfter) {
   this->__toneEnd();
-
-  if (_delay) {
-    this->__delay(_delay);
+  if (_delayAfter > 0) {
+    this->__delay(_delayAfter);
   }
 }
 
-void BUZZER::question(uint32_t _delayAfter) {
-  for (byte i = 1; i < 4; i++) {
+void BUZZER::question(uint16_t _delayAfter) {
+  for (uint8_t i = 1; i < 4; i++) {
     this->__toneStart(500 * i);
     this->__delay(100);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::no(uint32_t _delayAfter) {
-  for (byte i = 0; i < 2; i ++) {
+void BUZZER::no(uint16_t _delayAfter) {
+  for (uint8_t i = 0; i < 2; i++) {
     this->__toneStart(500);
     this->__delay(100);
     this->__toneEnd();
     this->__delay(100);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::ok(uint32_t _delayAfter) {
-  for (word i = 2000; i <= 2500; i += 100) {
-    this->__toneStart(i);
+void BUZZER::ok(uint16_t _delayAfter) {
+  for (uint16_t freq = 2000; freq <= 2500; freq += 100) {
+    this->__toneStart(freq);
     this->__delay(100);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::cancel(uint32_t _delayAfter) {
-  for (word i = 2500; i >= 2000; i -= 100) {
-    this->__toneStart(i);
+void BUZZER::cancel(uint16_t _delayAfter) {
+  for (uint16_t freq = 2500; freq >= 2000; freq -= 100) {
+    this->__toneStart(freq);
     this->__delay(100);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::alarm(uint32_t _delayAfter) {
-  for (byte i = 0; i < 2; i++) {
-    for (byte x = 0; x < 180; x++) {
-      double sinValue  = sin(x * (3.1412 / 180));
-      word   frequency = 2000 + (word)(sinValue * 1000);
-      this->__toneStart(frequency);
+void BUZZER::alarm(uint16_t _delayAfter) {
+  for (uint8_t i = 0; i < 4; i++) {
+    for (uint16_t f = 1000; f < 3000; f += 40) {
+      this->__toneStart(f);
+      delayMicroseconds(500);
     }
-    this->__delay(100);
+    for (uint16_t f = 3000; f > 1000; f -= 40) {
+      this->__toneStart(f);
+      delayMicroseconds(500);
+    }
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::done(uint32_t _delayAfter) {
+void BUZZER::done(uint16_t _delayAfter) {
   this->__toneStart(1500);
   this->__delay(100);
   this->__toneStart(2500);
   this->__delay(100);
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::refuse(uint32_t _delayAfter) {
-  for (word i = 0; i < 5; i++) {
+void BUZZER::refuse(uint16_t _delayAfter) {
+  for (uint8_t i = 0; i < 5; i++) {
     this->__toneStart(775);
     this->__delay(100);
     this->__toneEnd();
     this->__delay(100);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::ping(uint32_t _delayAfter) {
+void BUZZER::in(uint16_t _delayAfter) {
+  for (uint16_t i = 700; i < 2000; i += 10) {
+    this->__toneStart(i);
+    delayMicroseconds(800);
+  }
+  this->__end(_delayAfter);
+}
+
+void BUZZER::out(uint16_t _delayAfter) {
+  for (uint16_t i = 2000; i > 700; i -= 10) {
+    this->__toneStart(i);
+    delayMicroseconds(800);
+  }
+  this->__end(_delayAfter);
+}
+
+void BUZZER::ping(uint16_t _delayAfter) {
   this->__toneStart(1000);
   this->__delay(100);
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::pong(uint32_t _delayAfter) {
+void BUZZER::pong(uint16_t _delayAfter) {
   this->__toneStart(500);
   this->__delay(100);
-  
   this->__end(_delayAfter);
 }
 
-void BUZZER::in(uint32_t _delayAfter) {
-  for(word i = 700; i < 2000; i++){
-    this->__toneStart(i);
-    this->__delay(0, 10);
+void BUZZER::error(uint16_t _delayAfter) {
+  this->__toneStart(250);
+  this->__delay(300);
+  this->__end(_delayAfter);
+}
+
+void BUZZER::click(uint16_t _delayAfter) {
+  for (uint8_t i = 0; i < 2; i++) {
+    this->__toneStart(3000);
+    this->__delay(10);
+    this->__toneEnd();
+    this->__delay(50);
   }
-
   this->__end(_delayAfter);
 }
 
-void BUZZER::out(uint32_t _delayAfter) {
-  for(word i = 2000; i > 700; i--){
-    this->__toneStart(i);
-    this->__delay(0, 10);
+void BUZZER::notify(uint16_t _delayAfter) {
+  for (uint16_t f = 1500; f <= 2500; f += 500) {
+    this->__toneStart(f);
+    this->__delay(50);
   }
-
   this->__end(_delayAfter);
 }
 
-#endif
+void BUZZER::lowBat(uint16_t _delayAfter) {
+  this->__toneStart(1500);
+  this->__delay(150);
+  this->__toneStart(800);
+  this->__delay(250);
+  this->__end(_delayAfter);
+}
+
+void BUZZER::startup(uint16_t _delayAfter) {
+  uint16_t notes[] = {1318, 1568, 2093, 2637}; // Mi, Sol, Do, Mi
+  for (uint8_t i = 0; i < 4; i++) {
+    this->__toneStart(notes[i]);
+    this->__delay(100);
+  }
+  this->__end(_delayAfter);
+}
+
+void BUZZER::sleep(uint16_t _delayAfter) {
+  this->__toneStart(1500);
+  this->__delay(200);
+  this->__toneStart(1000);
+  this->__delay(300);
+  this->__toneStart(500);
+  this->__delay(400);
+  this->__end(_delayAfter);
+}
+
+void BUZZER::critical(uint16_t _delayAfter) {
+  for (uint8_t i = 0; i < 6; i++) {
+    this->__toneStart(3500);
+    this->__delay(50);
+    this->__toneEnd();
+    this->__delay(50);
+  }
+  this->__end(_delayAfter);
+}
+
+void BUZZER::scan(uint16_t _delayAfter) {
+  this->__toneStart(2000);
+  this->__delay(60);
+  this->__toneStart(3000);
+  this->__delay(60);
+  this->__end(_delayAfter);
+}
+
+void BUZZER::key(uint16_t _delayAfter) {
+  this->__toneStart(800);
+  this->__delay(15);
+  this->__end(_delayAfter);
+}
